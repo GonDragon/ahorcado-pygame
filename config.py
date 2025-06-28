0e7adb29bc67892ba7e55c92e41c655af85bb6ef
GermanJavierR <german.j.r1002@gmail.com>
2025-06-28 18:18:41 -0300

    agregando una constante llamada VERDE

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, os
 
 pygame.init()
 
@@ -13,13 +13,16 @@ pygame.display.set_caption("Juego del Ahorcado: G-Team Edition")
 BLANCO = (255, 255, 255)
 NEGRO = (0, 0, 0)
 ROJO = (255, 0, 0)
+VERDE = (0, 255, 0)
 
 # ----------------- FUENTE -----------------
-FUENTE = pygame.font.SysFont(None, 48)
+FUENTE = pygame.font.SysFont(None, 44)
 
 # ----------------- SONIDO -----------------
 pygame.mixer.init()  # Inicializa el motor de sonido
-sonido_error = pygame.mixer.Sound("recursos\\error.wav")  # Asegurate de tener este archivo
+sonido_error = pygame.mixer.Sound(os.path.join(os.getcwd(), "recursos","error.wav"))  # Asegurate de tener este archivo
+sonido_moneda = pygame.mixer.Sound(os.path.join(os.getcwd(), "assets/sonidos/coin.wav"))  # Asegurate de tener este archivo
+sonido_moneda.set_volume(0.2)  # Ajusta el volumen del sonido de la moneda (0.0 a 1.0)
 
 # ------------------ CONFIGURACIONES DE JUEGO -------------------
 INTENTOS_MAXIMOS = 6
